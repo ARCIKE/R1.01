120a25af0d21386394c142cc282d6ec4f993efb9
Alex <64358227+ARCIKE@users.noreply.github.com>
2022-10-09 19:17:06 +0200

    Update

--- TP6/src/ExoThree.java
+++ TP6/src/ExoThree.java
@@ -1,4 +1,9 @@
+
 public class ExoThree {
+
+    void principal() {
+        testEliminerDouble();
+    }
 
     /*** élimine les valeurs en plusieurs exemplaires dans un tableau
      * un élément présent plusieurs fois n’est plus qu’en un seul exemplaire
@@ -7,21 +12,34 @@ public class ExoThree {
      **/
     int eliminerDouble(int[] tab) {
         int length = tab.length;
-        int[] number = new int[length];
-
-        return number.length;
-    }
-
-    boolean checkIfIn (int[] tab, int number) {
-        boolean result = false;
-
-        for (int i = 0; i < tab.length; i++) {
-            if (tab[i] == number) {
-                result = true;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (tab[i] == tab[j]) {
+                    for (int k = j; k < length - 1; k++) {
+                        tab[k] = tab[k + 1];
+                    }
+                    j--;
+                    length--;
+                }
             }
         }
-        return result;
+        return length;
     }
-    
 
+    void testEliminerDouble() {
+        System.out.println("testEliminerDouble");
+        System.out.println("eliminerDouble(e ({0, 0, 2, 3, 0, 2, 1, 3, 3, 0}) = 4");
+        int[] tab = {0, 0, 2, 3, 0, 2, 1, 3, 3, 0};
+        testCasEliminerDouble(tab, 4);
+
+    }
+
+    void testCasEliminerDouble(int[] tab, int expected) {
+        int result = eliminerDouble(tab);
+        if (result == expected) {
+            System.out.println("\u001B[32m Test réussi \u001B[0m");
+        } else {
+            System.out.println("Test échoué");
+        }
+    }
 }
